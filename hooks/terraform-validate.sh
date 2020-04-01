dd767c99b238cce0541d7a3700944639dea2bc5a
Dimitris Stafylarakis <dimitris.stafylarakis@linkit.nl>
2020-04-01 21:35:30 +0200

    add setup-file argument to terraform-validate

--- hooks/terraform-validate.sh
+++ hooks/terraform-validate.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-set -e
+while [[ $1 = --* ]]; do
+  case $1 in
+    --setup-file) setupFile="$2"; shift ;;
+  esac
+  shift
+done
 
 for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
   terraform init -backend=false "$dir"
+  # fetch file for validation setup
+  [[ -f "$setupFile" ]] && { echo "fetching $setupFile"; cp "$setupFile" "$dir"; }
   terraform validate "$dir"
+  # validation teardown
+  [[ -f "$setupFile" ]] && { rm "$dir/${setupFile##*/}"; }
 done
