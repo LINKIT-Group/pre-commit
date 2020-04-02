1cd6d41011a93276f4b6ef7058b79240bb2ba5ea
Dimitris Stafylarakis <dimitris.stafylarakis@linkit.nl>
2020-04-02 15:07:33 +0200

    add shellcheck

---
+++ hooks/shellcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+exit_status=0
+
+for file in "$@"; do
+  if (head -1 "$file" | grep '^#!.*sh'>/dev/null); then
+    if ! shellcheck "$file"; then
+      exit_status=1
+    fi
+  elif [[ "$file" =~ \.sh$|bash$ ]]; then
+    echo "$file: missing shebang"
+    exit_status=1
+  fi
+done
+
+exit $exit_status
