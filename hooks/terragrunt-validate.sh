759749b8160bbe9d8452c5b36f4905f2845a954a
Hamidreza Josheghani <hr.josheghani@gmail.com>
2020-04-09 13:58:01 +0200

    MLOPS-43, minor fixes, adding terragrunt validate

--- hooks/terragrunt-validate.sh
+++ hooks/terragrunt-validate.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-for i in "$@"; do
-  case $i in
-    --setup-file=*) setupFile="${i#*=}"; echo $setupFile; exit; ;;
-  esac
-  shift
-done
-
-for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
-  terragrunt init -backend=false "$dir"
-  # fetch file for validation setup
-  [[ -f "$setupFile" ]] && { echo "fetching $setupFile"; cp "$setupFile" "$dir"; }
-  terragrunt validate "$dir"
-  # validation teardown
-  [[ -f "$setupFile" ]] && { rm "$dir/${setupFile##*/}"; }
+# Run terragrunt validate only on directories which include terragrunt.hcl
+for dir in $(echo "$@" | grep terragrunt.hcl | xargs -n1 dirname | sort -u | uniq); do
+  terragrunt validate
 done
