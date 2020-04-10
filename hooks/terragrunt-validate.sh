ac8a1e26faad58093ac5b0a5c90cb84d4e5e1725
Hamidreza Josheghani <hr.josheghani@gmail.com>
2020-04-10 11:08:56 +0200

    MLOPS-49, dir parameter was missed

--- hooks/terragrunt-validate.sh
+++ hooks/terragrunt-validate.sh
@@ -2,5 +2,5 @@
 
 # Run terragrunt validate for all on directories which include env.hcl
 for dir in $(echo "$@" | grep env.hcl | xargs -n1 dirname | sort -u | uniq); do
-  terragrunt validate-all
+  terragrunt validate-all "$dir"
 done
