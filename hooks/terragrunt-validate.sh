5525989178b62ec4860dd0c6b477145a6b20954c
Hamidreza Josheghani <hr.josheghani@gmail.com>
2020-04-10 11:20:32 +0200

    MLOPS-49, use pre-commit-terraform

--- hooks/terragrunt-validate.sh
+++ hooks/terragrunt-validate.sh
@@ -1,6 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-# Run terragrunt validate for all on directories which include env.hcl
-for dir in $(echo "$@" | grep env.hcl | xargs -n1 dirname | sort -u | uniq); do
-  terragrunt validate-all "$dir"
+declare -a paths
+index=0
+error=0
+
+for file_with_path in "$@"; do
+  file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+
+  paths[index]=$(dirname "$file_with_path")
+  (("index+=1"))
 done
+
+for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
+  path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+
+  if [[ -n "$(find $path_uniq -maxdepth 1 -name '*.terragrunt' -print -quit)" ]]; then
+
+    starting_path=$(realpath "$path_uniq")
+    terragrunt_path="$path_uniq"
+
+    # Find the relevant .terragrunt directory (indicating a 'terragrunt init'),
+    # but fall through to the current directory.
+    while [[ "$terragrunt_path" != "." ]]; do
+      if [[ -d "$terragrunt_path/.terraform" ]]; then
+        break
+      else
+        terragrunt_path=$(dirname "$terragrunt_path")
+      fi
+    done
+
+    validate_path="${path_uniq#"$terragrunt_path"}"
+
+    # Change to the directory that has been initialized, run validation, then
+    # change back to the starting directory.
+    cd "$(realpath "$terragrunt_path")"
+    if ! terragrunt validate $validate_path; then
+      error=1
+      echo
+      echo "Failed path: $path_uniq"
+      echo "================================"
+    fi
+    cd "$starting_path"
+  fi
+done
+
+if [[ "${error}" -ne 0 ]]; then
+  exit 1
+fi
